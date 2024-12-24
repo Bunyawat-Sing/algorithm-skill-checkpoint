4c2ecc38b678a025b95aaba104aee8c65f8efd84
Bunyawat Singkiporn <pupahero@gmail.com>
2024-12-24 14:19:52 +0700

    q-3

--- question-3.js
+++ question-3.js
@@ -1,20 +1,20 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: O(n) because use linear search
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: O(n^2) because use binary search
 
 
-*/
+*/ 
 
 /* 
 Which function is more efficient and why?
-Answer:
+Answer: findProductPrice because have Big O = O(n) that have shorter time to compile compare to findProductPrice (O(n^2))
 
 
 */
