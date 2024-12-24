4047b53a34e44b116c68eada7077141006354a81
Bunyawat Singkiporn <pupahero@gmail.com>
2024-12-24 14:33:49 +0700

    final: q-5

--- question-3.js
+++ question-3.js
@@ -7,14 +7,14 @@ Answer: O(n) because use linear search
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer: O(n^2) because use binary search
+Answer: O(2n) because use binary search
 
 
-*/ 
+*/
 
 /* 
 Which function is more efficient and why?
-Answer: findProductPrice because have Big O = O(n) that have shorter time to compile compare to findProductPrice (O(n^2))
+Answer: findProductPrice because have Big O = O(n) that have shorter time to compile compare to findProductPrice (O(2n))
 
 
 */
